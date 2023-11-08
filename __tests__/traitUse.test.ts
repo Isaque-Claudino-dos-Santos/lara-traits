61b3a932aaa975014c4fa82327b43a84186ead98
Isaque dos Santos <74436802+Isaque-Claudino-dos-Santos@users.noreply.github.com>
2023-11-08 01:05:42 +0000

    add error

--- __tests__/traitUse.test.ts
+++ __tests__/traitUse.test.ts
@@ -3,23 +3,31 @@ import { faker } from "@faker-js/faker";
 
 describe("Testing method use", () => {
   test("Should add variable in object", () => {
-    const key = "name";
-    const value = faker.person.firstName();
-
+    const key1 = "name1";
+    const value1 = faker.person.firstName();
+    const key2 = "name1";
+    const value2 = faker.person.firstName();
     class A {
-      [key] = value;
+      [key1] = value1;
     }
 
-    interface B extends A {}
     class B {
+      [key2] = value2;
+    }
+
+    interface C extends A, B {}
+    class C {
       constructor() {
-        use(this, [A]);
+        use(this, [A, B]);
       }
     }
 
-    const b = new B();   
-    
-    expect(key in b).toBeTruthy();
-    expect(b[key]).toEqual(value);
+    const c = new C();
+
+    expect(key1 in c).toBeTruthy();
+    expect(c[key1]).toEqual(value1);
+
+    expect(key2 in c).toBeTruthy();
+    expect(c[key2]).toEqual(value2);
   });
 });
