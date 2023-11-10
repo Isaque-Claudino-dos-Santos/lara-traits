7593dbc446b043a4636b5bc50be43e677cad112f
Isaque dos Santos <74436802+Isaque-Claudino-dos-Santos@users.noreply.github.com>
2023-11-10 17:27:24 +0000

    use new test

--- __tests__/traitUse.test.ts
+++ __tests__/traitUse.test.ts
@@ -1,24 +1,19 @@
-import use from "../src/traitUse";
+import trait from "../src/index.ts";
 import { faker } from "@faker-js/faker";
 
 describe("Testing method use", () => {
-  test("Should add variable in object", () => {
+  test("Should add only one variable in object", () => {
     const keyName = "name";
     const nameValue = faker.person.firstName();
-    const keyAge = "age";
-    const ageValue = faker.number.int({ max: 3 });
+
     class A {
       [keyName] = nameValue;
     }
 
-    class B {
-      [keyAge] = ageValue;
-    }
-
-    interface C extends A, B {}
+    interface C extends A {}
     class C {
       constructor() {
-        use(this, [A, B]);
+        trait.use(this, [A]);
       }
     }
 
@@ -26,8 +21,5 @@ describe("Testing method use", () => {
 
     expect(keyName in c).toBeTruthy();
     expect(c[keyName]).toEqual(nameValue);
-
-    expect(keyAge in c).toBeTruthy();
-    expect(c[keyAge]).toEqual(ageValue);
   });
 });
