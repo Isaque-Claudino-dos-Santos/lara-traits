5f6de513df1bade76ea0c0f77b5084e3cbcfa06e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-11-12 20:30:54 -0300

    add return target type

--- src/TraitInstanceOf.ts
+++ src/TraitInstanceOf.ts
@@ -2,7 +2,10 @@ import { Constructor, Target } from './Types'
 import { PROP_CONSTRUCTORS } from './bootstrep'
 
 export default class TraitInstanceOf {
-  instanceOf(target: Target, trait: Constructor<object>) {
+  instanceOf(
+    target: Target,
+    trait: Constructor<object>
+  ): target is typeof trait {
     if (!(PROP_CONSTRUCTORS in target)) return false
     const constructors = target[PROP_CONSTRUCTORS] as Constructor<object>[]
 
