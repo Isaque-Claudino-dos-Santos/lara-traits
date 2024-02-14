e3c57a3d121558680ea887995a6eaf1b57eeec50
Isaque dos Santos <74436802+Isaque-Claudino-dos-Santos@users.noreply.github.com>
2024-02-14 17:56:18 +0000

    fix: fixed data type names

--- src/TraitInstanceOf.ts
+++ src/TraitInstanceOf.ts
@@ -1,13 +1,10 @@
-import { Constructor, Target } from './Types'
+import { Constructor } from './Types'
 import { PROP_CONSTRUCTORS } from './bootstrep'
 
 export default class TraitInstanceOf {
-  instanceOf(
-    target: Target,
-    trait: Constructor<object>
-  ): target is typeof trait {
+  instanceOf(target: object, trait: Constructor): target is typeof trait {
     if (!(PROP_CONSTRUCTORS in target)) return false
-    const constructors = target[PROP_CONSTRUCTORS] as Constructor<object>[]
+    const constructors = target[PROP_CONSTRUCTORS] as Constructor[]
 
     for (const constructor of constructors) {
       if (trait === constructor) return true
